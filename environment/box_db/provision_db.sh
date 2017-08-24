c0aeee27f69be4f6dc6e168b89ecefa642e617fa
Fane <fane247@gmail.com>
2017-08-24 15:08:34 +0100

    changed quite a bit, mostly the database.yml

--- environment/box_db/provision_db.sh
+++ environment/box_db/provision_db.sh
@@ -5,5 +5,12 @@ sudo apt-get -y install postgresql postgresql-contrib libpq-dev
 # sudo -i -u postgres
 cd app
 sudo rm /etc/postgresql/9.5/main/postgresql.conf
-sudo cp postgresql.conf /etc/postgresql/9.5/main/postgresql.conf
+sudo cp /home/ubuntu/app/environment/box_db/postgresql.conf /etc/postgresql/9.5/main/postgresql.conf
+
+
+sudo rm /etc/postgresql/9.5/main/pg_hba.conf
+sudo cp /home/ubuntu/app/environment/templates/pg_hba.conf /etc/postgresql/9.5/main/pg_hba.conf
+
 sudo service postgresql restart
+
+
