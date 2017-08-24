5bef3711f99ea5f096c6053a35d9a38739671d30
Fane <fane247@gmail.com>
2017-08-24 16:28:34 +0100

    Removed nginx from provision file

--- environment/box_app/provision_app.sh
+++ environment/box_app/provision_app.sh
@@ -8,19 +8,6 @@ sudo gem install rake
 sudo gem install rails
 
 sudo apt-get -y install postgresql postgresql-contrib libpq-dev
-#
-sudo apt-get install -y dirmngr gnupg
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 561F9B9CAC40B2F7
-sudo apt-get install -y apt-transport-https ca-certificates
-# Add our APT repository
-sudo sh -c 'echo deb https://oss-binaries.phusionpassenger.com/apt/passenger xenial main > /etc/apt/sources.list.d/passenger.list'
-sudo apt-get update
-# Install Passenger + Nginx
-yes N | sudo apt-get install -y nginx-extras passenger
-# sudo apt-get install nginx -y
 
-sudo rm /etc/nginx/sites-available/default
-sudo cp /home/ubuntu/app/environment/templates/default /etc/nginx/sites-available/default
-sudo cp /home/ubuntu/app/environment/templates/nginx.conf /etc/nginx/nginx.conf
 sudo chmod +x /home/ubuntu/app
 sudo service nginx restart
