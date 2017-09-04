9c11169c25dc2a1c5071edeff68cb268adccf3a0
Stephen Reid <steveyblam@hotmail.co.uk>
2017-09-04 11:14:19 +0100

    reversed order of logs

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -8,7 +8,7 @@ class LogsController < ApplicationController
   # GET /logs
   # GET /logs.json
   def index
-    @logs = Log.order(:created_at)
+    @logs = Log.order(created_at: :desc)
     @logs = @logs.map do |log|
       populate_log log
     end
