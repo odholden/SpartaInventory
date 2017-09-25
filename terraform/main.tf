adcf9d789e65ee629aa2709a7727a4f5356f387b
Divesh Mavji <divesh987@hotmail.co.uk>
2017-09-25 16:24:45 +0100

    pull req changes made

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,24 +4,24 @@ provider "aws" {
 
 resource "aws_vpc" "inventory-vpc" {
   tags {
-      Name = "Inventory - VPC"
+    Name = "Inventory - VPC"
   }
   cidr_block = "11.3.0.0/16"
-  
 }
-
-
-
 
 resource "aws_instance" "inventory-web" {
   ami =   "ami-996372fd"
   instance_type = "t2.micro"
-
+  
   tags {
     Name = "web-inventory"
   }
 }
 
+resource "random_string" "password" {
+  length = 16
+  special = true
+}
 
 resource "aws_db_instance" "inventory-db" {
   engine               = "postgres"
@@ -29,7 +29,7 @@ resource "aws_db_instance" "inventory-db" {
   allocated_storage = 8
   name = "spartaInventoryDb"
   username ="inventory"
-  password = "sparta123"
+  password =  ${random_string.result}
   
   tags {
     Name = "db-inventory"
