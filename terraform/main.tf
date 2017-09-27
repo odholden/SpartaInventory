7d1099880cdae4741fba7383fda8e499b1953d19
Divesh Mavji <divesh987@hotmail.co.uk>
2017-09-27 14:35:39 +0100

    indented

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,13 +1,13 @@
 provider "aws" {
-  region  = "eu-west-2"
-  
+  region  = "eu-west-2" 
 }
 
 resource "aws_vpc" "inventory-vpc" {
+  cidr_block = "11.3.0.0/16"
+  
   tags {
     Name = "Inventory - VPC"
   }
-  cidr_block = "11.3.0.0/16"
 }
 
 resource "aws_internet_gateway" "default" {
@@ -33,6 +33,7 @@ resource "aws_subnet" "elb-subnet" {
   vpc_id = "${aws_vpc.inventory-vpc.id}"
   cidr_block = "11.3.3.0/24"
   map_public_ip_on_launch = true
+
   tags {
     Name = "inventory-elb"
   }
@@ -43,6 +44,7 @@ resource "aws_subnet" "inventory-web" {
   vpc_id = "${aws_vpc.inventory-vpc.id}"
   cidr_block = "11.3.1.0/24"
   map_public_ip_on_launch = false
+
   tags {
     Name = "inventory-app"
   }
@@ -52,6 +54,7 @@ resource "aws_subnet" "inventory-db" {
   vpc_id = "${aws_vpc.inventory-vpc.id}"
   cidr_block = "11.3.2.0/24"
   map_public_ip_on_launch = false
+
   tags {
     Name = "inventory-db"
   }
@@ -61,26 +64,27 @@ resource "aws_security_group" "inventory-sg-elb"  {
   description = "Allow all inbound traffic through port 80 and 443."
   vpc_id = "${aws_vpc.inventory-vpc.id}"
 
-  ingress{
+  ingress {
     from_port       = 80
     to_port         = 80
     protocol        = "tcp"
     cidr_blocks     = ["0.0.0.0/0"]
   }
 
-  ingress{
+  ingress {
     from_port       = 443
     to_port         = 443
     protocol        = "tcp"
     cidr_blocks     = ["0.0.0.0/0"]
   }
 
-  egress{
+  egress {
     from_port       = 3000
     to_port         = 3000
     protocol        = "0"
     cidr_blocks     = ["0.0.0.0/0"]
   }
+
   tags {
     Name = "inventory-sg-elb"
   }
@@ -91,19 +95,20 @@ resource "aws_security_group" "inventory-sg-app"  {
   description = "Allow all inbound traffic through port 3000 only"
   vpc_id = "${aws_vpc.inventory-vpc.id}"
 
-  ingress{
+  ingress {
     from_port       = 3000
     to_port         = 3000
     protocol        = "tcp"
     security_groups = ["${aws_security_group.inventory-sg-elb.id}"]
   }
 
-  egress{
+  egress {
     from_port       = 0
     to_port         = 0
     protocol        = "-1"
     cidr_blocks     = ["0.0.0.0/0"]
   }
+
   tags {
     Name = "inventory-SecurityGroup-app"
   }
@@ -114,19 +119,20 @@ resource "aws_security_group" "inventory-sg-db"  {
   description = "Allow all inbound traffic through port 5432 only"
   vpc_id = "${aws_vpc.inventory-vpc.id}"
 
-  ingress{
+  ingress {
     from_port       = 5432
     to_port         = 5432
     protocol        = "tcp"
     security_groups = ["${aws_security_group.inventory-sg-app.id}"]
   }
 
-  egress{
+  egress {
     from_port       = 0
     to_port         = 0
     protocol        = "-1"
     cidr_blocks     = ["0.0.0.0/0"]
   }
+
   tags {
     Name = "inventory-SecurityGroup-db"
   }
@@ -136,7 +142,6 @@ resource "aws_elb" "elb" {
   name = "inventory-elb"
   subnets = ["${aws_subnet.inventory-web.id}"]
   security_groups = ["${aws_security_group.inventory-sg-elb.id}"]
-  # availability_zones = ["eu-west-2a"]
 
   listener {
     instance_port = 3000
@@ -161,6 +166,7 @@ resource "aws_instance" "inventory-web" {
   ami =   "ami-996372fd"
   instance_type = "t2.micro"
   subnet_id = "${aws_subnet.inventory-web.id}"
+
   tags {
     Name = "web-inventory"
   }
@@ -174,7 +180,6 @@ resource "random_string" "password" {
 resource "aws_db_instance" "inventory-db" {
   engine               = "postgres"
   instance_class =  "db.t2.micro"
-  # db_subnet_group_name = "inventory-db"
   allocated_storage = 8
   name = "spartaInventoryDb"
   username ="inventory"
