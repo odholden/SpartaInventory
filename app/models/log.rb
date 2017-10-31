24d017bb0f92784b64f0e466d37df13061a38bc5
Stephen Reid <steveyblam@hotmail.co.uk>
2017-10-31 09:50:24 +0000

    hotfix: due date default set

--- app/models/log.rb
+++ app/models/log.rb
@@ -2,4 +2,10 @@ class Log < ApplicationRecord
   belongs_to :item
 
   attr_accessor :borrower, :lender , :returned_to
+
+  after_initialize do
+    if self.new_record?
+      self.due_date = Date.today + 1
+    end
+  end
 end
