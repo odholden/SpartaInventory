c7f7a7bb6e4d6b3b73a0505ff1f710b9947ebbb8
ahawkins94 <adamhawkins94@hotmail.co.uk>
2017-11-17 16:57:05 +0000

    created superclass and added comments - hopefully it is clear enough

---
+++ lib/inventory_object_model.rb
@@ -0,0 +1,5 @@
+# at the top of the file, require the file path for each page classes
+
+class InventoryApp
+# this is the superclass. This will contain sub classes that contain methods that navigate the pages.
+end
