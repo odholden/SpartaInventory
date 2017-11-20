0cec4f5741de55c6f93ae6c53296b86c9752dcb0
MashR3 <michaelash17@googlemail.com>
2017-11-20 13:57:05 +0000

    step dep scaffolding created

---
+++ features/step_definitions/log_in_out_step_def.rb
@@ -0,0 +1,45 @@
+Given("I have access to the login page") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("I enter in the required fields") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("I submit the credentials") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+
+Then("I am taken to the logs page") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I have access to restricted resources") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("I enter in the incorrect details on the login page") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I am returned to the login page") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("an error message is displayed") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given("I am already logged in") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("I click the logout button") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I am taken to the login page") do
+
+  pending # Write code here that turns the phrase above into concrete actions
+end
---
+++ features/step_definitions/user_create_item.rb
@@ -0,0 +1,90 @@
+Given("I am already logged in") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("I click the logout button") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I am taken to the login page") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("I click on the items link") do
+  pending # Write code here that turns the phrase above into concrete actions
+
+end
+
+Then("I am taken to the items page") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I can see all of the items in the database") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given("I am on the create new items page") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("I enter in a description and a serial") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("I click save item") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("a record is created") do
+  pending # Write code here that turns the phrase above into concrete actions
+
+end
+
+Then("the record is displayed") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given("I am on the checkout item page") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("I select an item and a borrower") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("click the checkout item button") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("a log is created") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I am taken to the show log page") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given("I am on the logs page") do
+
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("I click check-in for a log") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("confirm by clicking ok") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("the returned to and return date are set") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("the log appears as returned") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I am returned to the logs page") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
